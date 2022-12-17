0ea0420092bc1c165ce20ce04dcab159c836ba82
Dennis Veeneman <dennis_veeneman@hotmail.com>
2022-12-17 13:34:06 +0100

    logo's

--- resources/views/page/info.blade.php
+++ resources/views/page/info.blade.php
@@ -1,1 +1,73 @@
-@extends('app')@section('title', 'Praktische informatie')@section('content')<div class="blocked-page">		<div class="row">		<div class="block text">			<h2>Toegang</h2>			<p>Het Carbid Festival is voor jong en oud toegankelijk. We vragen geen entree. Natuurlijk is het wel zo dat er de nodige kosten worden gemaakt voor het festival. Een gift stellen we dan ook zeer op prijs.</p>			<p>We zijn &#39;geopend&#39; op 31 december tussen 13.00 en 18.00 uur.</p>		</div>		<div class="block image cover">			<img src="img/zone-eigen-risico.jpg" alt="Zone eigen risico" />		</div>	</div>		<div class="row">		<div class="block image cover">			<img src="img/bar.jpg" alt="De bar is tussen 13:00 en 18:00 geopend!" />		</div>		<div class="block text">			<h2>Eten en drinken</h2>			<p>In de feesttent is er de mogelijkheid om een hapje en drankje te bestellen. Dit kan uiteraard niet voor niets. Een biertje kost bijvoorbeeld €2,00. We accepteren ook de munten die eerdere edities zijn gekocht op het Carbid Fesitval.</p>		</div>	</div>		<div class="row">		<div class="block text">			<h2>Veiligheid</h2>			<p>Veiligheid gaat boven alles. Om het Carbid Festival op een veilige manier te laten verlopen zijn de melkbussen in een stalen stellage geplaatst zodat ze geen kant op kunnen. Verder is deze stellage geplaatst op een veilige afstand van de feesttent. Betreden van het festivalterrein geschiedt op eigen risico. Het carbid schieten wordt alleen door de organisatie gedaan. Tip: neem gehoorbescherming mee!</p>		</div>		<div class="block image cover">			<img src="img/veiligheid.jpg" alt="Gepaste afstand tot de bussen in soms wel handig" />		</div>	</div>		<div class="row">		<div class="block image cover">			<img src="img/bijna-klaar-groepsfoto.jpg" alt="Bijna klaar voor de groepsfoto" />		</div>		<div class="block text">			<h2>Organisatie</h2>			<p>Het Carbid Festival wordt georganiseerd door twee vriendengroepen uit Gorssel. De leden zijn afkomstig uit Epse, Harfsen, Joppe en Gorssel.</p>			<p>Iedereen werkt mee op vrijwillige basis om er een mooi feest van te maken.</p>		</div>	</div>		<div class="row">		<div class="block text">			<h2>Bereikbaarheid</h2>			<p>Het festivalterrein is gelegen de Hassinklaan net buiten Epse, parkeren kan op het parkeerterrein van de sporthal.<br />				Geadviseerd wordt om met de fiets te komen, dan hoef je ook niet van en naar de parkeerplaats te lopen!</p>		</div>		<div class="block image cover">			<img src="img/ingang.jpg" alt="Ingang over 25 meter" />		</div>	</div>		<div class="row">		<div class="block image map">			<iframe style="border: none; width: 100%; height: 295px;" src="https://www.openstreetmap.org/export/embed.html?bbox=6.193274259567262%2C52.21617262085548%2C6.203606128692628%2C52.221082668893715&amp;layer=mapnik" width="300" height="295" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>		</div>		<div class="block text">			<h2>Locatie</h2>			<p>Het Carbid Festival vind plaats aan de Hassinklaan nabij de Reelaan.</p>			<p>Het advies is om met de fiets te komen, u kunt dan het dichtst bij parkeren, en u hoeft dan geen discussies te hebben over wie er terug rijdt!</p>			<p><a href="https://www.google.nl/maps/dir//52.2187797,6.2005806/@52.2163082,6.1979306,16z/data=!4m2!4m1!3e2" target="_blank">Maak een routebeschrijving via Google Maps</a></p>		</div>	</div></div>@endsection
+@extends('app')
+
+@section('title', 'Praktische informatie')
+
+@section('content')
+<div class="blocked-page">
+	
+	<div class="row">
+		<div class="block text">
+			<h2>Toegang</h2>
+			<p>Het Carbid Festival is voor jong en oud toegankelijk. We vragen geen entree. Natuurlijk is het wel zo dat er de nodige kosten worden gemaakt voor het festival. Een gift stellen we dan ook zeer op prijs.</p>
+			<p>We zijn &#39;geopend&#39; op 31 december tussen 13.00 en 18.00 uur.</p>
+		</div>
+		<div class="block image cover">
+			<img src="img/zone-eigen-risico.jpg" alt="Zone eigen risico" />
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="block image cover">
+			<img src="img/bar.jpg" alt="De bar is tussen 13:00 en 18:00 geopend!" />
+		</div>
+		<div class="block text">
+			<h2>Eten en drinken</h2>
+			<p>In de feesttent is er de mogelijkheid om een hapje en drankje te bestellen. Dit kan uiteraard niet voor niets. Een biertje kost bijvoorbeeld €2,00. We accepteren ook de munten die eerdere edities zijn gekocht op het Carbid Fesitval.</p>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="block text">
+			<h2>Veiligheid</h2>
+			<p>Veiligheid gaat boven alles. Om het Carbid Festival op een veilige manier te laten verlopen zijn de melkbussen in een stalen stellage geplaatst zodat ze geen kant op kunnen. Verder is deze stellage geplaatst op een veilige afstand van de feesttent. Betreden van het festivalterrein geschiedt op eigen risico. Het carbid schieten wordt alleen door de organisatie gedaan. Tip: neem gehoorbescherming mee!</p>
+		</div>
+		<div class="block image cover">
+			<img src="img/veiligheid.jpg" alt="Gepaste afstand tot de bussen in soms wel handig" />
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="block image cover">
+			<img src="img/bijna-klaar-groepsfoto.jpg" alt="Bijna klaar voor de groepsfoto" />
+		</div>
+		<div class="block text">
+			<h2>Organisatie</h2>
+			<p>Het Carbid Festival wordt georganiseerd door twee vriendengroepen uit Gorssel. De leden zijn afkomstig uit Epse, Harfsen, Joppe en Gorssel.</p>
+			<p>Iedereen werkt mee op vrijwillige basis om er een mooi feest van te maken.</p>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="block text">
+			<h2>Bereikbaarheid</h2>
+			<p>Het festivalterrein is gelegen de Hassinklaan net buiten Epse, parkeren kan op het parkeerterrein van de sporthal.<br />
+				Geadviseerd wordt om met de fiets te komen, dan hoef je ook niet van en naar de parkeerplaats te lopen!</p>
+		</div>
+		<div class="block image cover">
+			<img src="img/ingang.jpg" alt="Ingang over 25 meter" />
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="block image map">
+			<iframe style="border: none; width: 100%; height: 295px;" src="https://www.openstreetmap.org/export/embed.html?bbox=6.193274259567262%2C52.21617262085548%2C6.203606128692628%2C52.221082668893715&amp;layer=mapnik" width="300" height="295" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+		</div>
+		<div class="block text">
+			<h2>Locatie</h2>
+			<p>Het Carbid Festival vind plaats aan de Hassinklaan nabij de Reelaan.</p>
+			<p>Het advies is om met de fiets te komen, u kunt dan het dichtst bij parkeren, en u hoeft dan geen discussies te hebben over wie er terug rijdt!</p>
+			<p><a href="https://www.google.nl/maps/dir//52.2187797,6.2005806/@52.2163082,6.1979306,16z/data=!4m2!4m1!3e2" target="_blank">Maak een routebeschrijving via Google Maps</a></p>
+		</div>
+	</div>
+</div>
+@endsection
